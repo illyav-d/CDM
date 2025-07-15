5af1cba15bd89ebfefb8c5c8ba96061022a7a9ca
Illya Verheyden <illya.verheyden94@gmail.com>
2025-07-15 14:20:55 +0200

    Update Program.cs
    Smoke testing idea to load all dynamics entities through connectionstring

--- samples/2-create-manifest/code-cs/create-manifest/Program.cs
+++ samples/2-create-manifest/code-cs/create-manifest/Program.cs
@@ -7,32 +7,82 @@ namespace create_manifest
     using Microsoft.CommonDataModel.ObjectModel.Enums;
     using Microsoft.CommonDataModel.ObjectModel.Storage;
     using Microsoft.CommonDataModel.ObjectModel.Utilities;
+    using Microsoft.PowerPlatform.Dataverse.Client;
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Threading.Tasks;
-
-    /*
-     * ---------------------------------------------------------------------------------------------------------------------------------------
-     * This sample is going to simulate the steps a tool would follow in order to create a new manifest document in some user storage folder 
-     * when the shapes of the entities in that folder are all taken from some public standards with no extensions or additions
-     * The steps are:
-     *      1. Create a temporary 'manifest' object at the root of the corpus that contains a list of the selected entities
-     *      2. Each selected entity points at an 'abstract' schema defintion in the public standards. These entity docs are too hard to
-     *          deal with because of abstractions and inheritence, etc. So to make things concrete, we want to make a 'resolved' version of
-     *          each entity doc in our local folder. To do this, we call CreateResolvedManifestAsync on our starting manifest
-     *          This will resolve everything and find all of the relationships between entities for us
-     *      3. Save the new documents
-     *  --------------------------------------------------------------------------------------------------------------------------------------
-     */
 
     class Program
     {
         static async Task Main(string[] args)
         {
-            // Make a corpus, the corpus is the collection of all documents and folders created or discovered while navigating objects and paths
+            Console.WriteLine("Stap 1: Ophalen metadata uit Dataverse en opslaan als solution-export.json");
+            await ExportDataverseMetadataAsync();
+
+            Console.WriteLine("Stap 2: Manifest maken vanuit solution-export.json");
+            await CreateManifestAsync();
+        }
+
+        private static async Task ExportDataverseMetadataAsync()
+        {
+            string connectionString = "AuthType=OAuth;Url=https://yourorg.crm.dynamics.com;ClientId=xxxx;ClientSecret=xxxx;TenantId=xxxx;";
+
+            var serviceClient = new ServiceClient(connectionString);
+            if (!serviceClient.IsReady)
+            {
+                Console.WriteLine("Verbinding met Dataverse mislukt.");
+                return;
+            }
+
+            Console.WriteLine("Verbinding met Dataverse succesvol.");
+
+            var solutionExport = new SolutionExport
+            {
+                SolutionName = "MySolution",
+                Entities = new List<SolutionEntity>()
+            };
+
+            // Ophalen van alle entiteiten metadata (inclusief attributen)
+            var request = new Microsoft.Xrm.Sdk.Messages.RetrieveAllEntitiesRequest()
+            {
+                EntityFilters = Microsoft.Xrm.Sdk.Metadata.EntityFilters.Entity | Microsoft.Xrm.Sdk.Metadata.EntityFilters.Attributes,
+                RetrieveAsIfPublished = true
+            };
+
+            var response = (Microsoft.Xrm.Sdk.Messages.RetrieveAllEntitiesResponse)serviceClient.Execute(request);
+
+            foreach (var entityMetadata in response.EntityMetadata)
+            {
+                var attributes = new List<SolutionAttribute>();
+
+                foreach (var attr in entityMetadata.Attributes)
+                {
+                    attributes.Add(new SolutionAttribute
+                    {
+                        Name = attr.LogicalName,
+                        Type = attr.AttributeType.ToString()
+                    });
+                }
+
+                solutionExport.Entities.Add(new SolutionEntity
+                {
+                    LogicalName = entityMetadata.LogicalName,
+                    Attributes = attributes
+                });
+            }
+
+            string json = JsonConvert.SerializeObject(solutionExport, Formatting.Indented);
+            File.WriteAllText("solution-export.json", json);
+
+            Console.WriteLine("Metadata geëxporteerd naar solution-export.json");
+        }
+
+        private static async Task CreateManifestAsync()
+        {
             var cdmCorpus = new CdmCorpusDefinition();
 
-            // set callback to receive error and warning logs.
             cdmCorpus.SetEventCallback(new EventCallback
             {
                 Invoke = (level, message) =>
@@ -43,79 +93,59 @@ namespace create_manifest
 
             Console.WriteLine("Configure storage adapters");
 
-            // Configure storage adapters to point at the target local manifest location and at the fake public standards
             string pathFromExeToExampleRoot = "../../../../../../";
 
             cdmCorpus.Storage.Mount("local", new LocalAdapter(pathFromExeToExampleRoot + "2-create-manifest/sample-data"));
-            cdmCorpus.Storage.DefaultNamespace = "local"; // local is our default. so any paths that start out navigating without a device tag will assume local
+            cdmCorpus.Storage.DefaultNamespace = "local";
 
-            // Fake cdm, normaly use the CDM Standards adapter
-            // Mount it as the 'cdm' device, not the default so must use "cdm:/folder" to get there
             cdmCorpus.Storage.Mount("cdm", new LocalAdapter(pathFromExeToExampleRoot + "example-public-standards"));
 
-            // Example how to mount to the ADLS.
-            // cdmCorpus.Storage.Mount("adls",
-            //    new ADLSAdapter(
-            // "<ACCOUNT-NAME>.dfs.core.windows.net", // Hostname.
-            // "/<FILESYSTEM-NAME>", // Root.
-            // "72f988bf-86f1-41af-91ab-2d7cd011db47",  // Tenant ID.
-            // "<CLIENT-ID>",  // Client ID.
-            // "<CLIENT-SECRET>" // Client secret.
-            // ));
+            Console.WriteLine("Lees solution-export.json in");
+            string solutionJson = File.ReadAllText("solution-export.json");
+            var solution = JsonConvert.DeserializeObject<SolutionExport>(solutionJson);
 
-            Console.WriteLine("Make placeholder manifest");
-            // Make the temp manifest and add it to the root of the local documents in the corpus
+            Console.WriteLine("Maak placeholder manifest");
             CdmManifestDefinition manifestAbstract = cdmCorpus.MakeObject<CdmManifestDefinition>(CdmObjectType.ManifestDef, "tempAbstract");
 
-            // Add each declaration, this example is about medical appointments and care plans
-            manifestAbstract.Entities.Add("Account", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/Account.cdm.json/Account");
-            manifestAbstract.Entities.Add("Address", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/Address.cdm.json/Address");
-            manifestAbstract.Entities.Add("CarePlan", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/CarePlan.cdm.json/CarePlan");
-            manifestAbstract.Entities.Add("CodeableConcept", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/CodeableConcept.cdm.json/CodeableConcept");
-            manifestAbstract.Entities.Add("Contact", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/Contact.cdm.json/Contact");
-            manifestAbstract.Entities.Add("Device", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/Device.cdm.json/Device");
-            manifestAbstract.Entities.Add("EmrAppointment", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/EmrAppointment.cdm.json/EmrAppointment");
-            manifestAbstract.Entities.Add("Encounter", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/Encounter.cdm.json/Encounter");
-            manifestAbstract.Entities.Add("EpisodeOfCare", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/EpisodeOfCare.cdm.json/EpisodeOfCare");
-            manifestAbstract.Entities.Add("Location", "cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/Location.cdm.json/Location");
+            // Dynamisch toevoegen van entities op basis van solution-export.json
+            foreach (var entity in solution.Entities)
+            {
+                string entityName = entity.LogicalName;
 
-            // Add the temp manifest to the root of the local documents in the corpus.
+                // Pas dit pad aan als jouw CDM schema’s elders staan
+                string cdmPath = $"cdm:/core/applicationCommon/foundationCommon/crmCommon/accelerators/healthCare/electronicMedicalRecords/{entityName}.cdm.json/{entityName}";
+
+                manifestAbstract.Entities.Add(entityName, cdmPath);
+            }
+
             var localRoot = cdmCorpus.Storage.FetchRootFolder("local");
             localRoot.Documents.Add(manifestAbstract);
 
-            // Create the resolved version of everything in the root folder too
             Console.WriteLine("Resolve the placeholder");
             var manifestResolved = await manifestAbstract.CreateResolvedManifestAsync("default", "");
 
-            // Add an import to the foundations doc so the traits about partitons will resolve nicely
             manifestResolved.Imports.Add("cdm:/foundations.cdm.json");
 
             Console.WriteLine("Save the documents");
-            foreach(CdmEntityDeclarationDefinition eDef in manifestResolved.Entities)
+            foreach (CdmEntityDeclarationDefinition eDef in manifestResolved.Entities)
             {
-                // Get the entity being pointed at
-                var localEDef = eDef;
-                var entDef = await cdmCorpus.FetchObjectAsync<CdmEntityDefinition>(localEDef.EntityPath, manifestResolved);
-                // Make a fake partition, just to demo that
+                var entDef = await cdmCorpus.FetchObjectAsync<CdmEntityDefinition>(eDef.EntityPath, manifestResolved);
+
                 var part = cdmCorpus.MakeObject<CdmDataPartitionDefinition>(CdmObjectType.DataPartitionDef, $"{entDef.EntityName}-data-description");
-                localEDef.DataPartitions.Add(part);
+                eDef.DataPartitions.Add(part);
                 part.Explanation = "not real data, just for demo";
 
-                // Define the location of the partition, relative to the manifest
                 var location = $"local:/{entDef.EntityName}/partition-data.csv";
                 part.Location = cdmCorpus.Storage.CreateRelativeCorpusPath(location, manifestResolved);
 
-                // Add trait to partition for csv params
                 var csvTrait = part.ExhibitsTraits.Add("is.partition.format.CSV", false) as CdmTraitReference;
                 csvTrait.Arguments.Add("columnHeaders", "true");
                 csvTrait.Arguments.Add("delimiter", ",");
-                
-                // Get the actual location of the partition file from the corpus
+
                 string partPath = cdmCorpus.Storage.CorpusPathToAdapterPath(location);
 
-                // Make a fake file with nothing but header for columns
                 string header = "";
-                foreach(CdmTypeAttributeDefinition att in entDef.Attributes)
+                foreach (CdmTypeAttributeDefinition att in entDef.Attributes)
                 {
                     if (header != "")
                         header += ",";
@@ -127,6 +157,27 @@ namespace create_manifest
             }
 
             await manifestResolved.SaveAsAsync($"{manifestResolved.ManifestName}.manifest.cdm.json", true);
+
+            Console.WriteLine("Manifest creatie afgerond.");
         }
+    }
+
+    // Zet deze classes eventueel apart in een eigen file / project
+    public class SolutionExport
+    {
+        public string SolutionName { get; set; }
+        public List<SolutionEntity> Entities { get; set; }
+    }
+
+    public class SolutionEntity
+    {
+        public string LogicalName { get; set; }
+        public List<SolutionAttribute> Attributes { get; set; }
+    }
+
+    public class SolutionAttribute
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
     }
 }
